f03d5bad35505998c54471c64a10fad7e2c95ace
codma <ruddud1123@naver.com>
2022-11-10 19:44:41 +0900

    router

---
+++ router-tutorial/src/About.jsx
@@ -0,0 +1,14 @@
+
+import qs from 'qs';
+
+const About = ({ history }) => {
+  console.log(history)
+
+  return (
+    <div>
+      디테일
+    </div>
+  )
+}
+
+export default About
--- router-tutorial/src/App.jsx
+++ router-tutorial/src/App.jsx
@@ -1,32 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Home from './Home'
+import About from './About'
+import Profile from './Profile'
+import { Link, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+        {/* <Route path="*" element={<Home />} /> */}
+      </Routes>
     </div>
   )
 }
